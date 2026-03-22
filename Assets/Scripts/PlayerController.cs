558618c0396bf57b64407b4476cf1fb4c16bdee0
CompSciOmanjuu <nhuvo2004@gmail.com>
2026-03-22 13:29:13 -0400

    Basic walking movement implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,43 @@
-using UnityEngine;
+// code sourced from Game Code Library on Youtube:
+// https://youtu.be/DQY62meLVCk?sl=jP6NgwXZxVCI7H
+// https://youtu.be/82U4ToJU-28?si=qv8jDpar-OB0Vcbc
 
-public class Movement
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
 {
-    
+    [SerializeField] private float moveSpeed = 4f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput; 
+    private Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        animator.SetBool("isWalking", true);
+
+        if (context.canceled)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+        
+        moveInput = context.ReadValue<Vector2>();
+
+        animator.SetFloat("InputX", moveInput.x);
+        animator.SetFloat("InputY", moveInput.y);
+    }
 }
